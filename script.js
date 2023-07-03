38f7584aaf080f029840967dceeb1a5c0c9b3e46
RahmaaEsmail <rahmaaesmail26@gmail.com>
2023-07-03 10:03:17 +0300

    update function set Score In Sorted Position and move some element into fun

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
-const firstNumber = document.querySelector('.num-one');
-const secondNumber = document.querySelector('.num-two');
-const operator = document.querySelector('.operator');
 const enterBtn = document.querySelector('.enter');
 const output = document.querySelector('.output');
 const numbers = document.querySelectorAll('.num');
 const scoreNumberEle = document.querySelector('.score-number');
 const clearBtn = document.querySelector('.clear');
 const submitBtn = document.querySelector('.submit-btn');
-const inputName = document.querySelector('input');
 let scoreNumber = 0, dataList = [] 
+
+const random = scoreNumber < 100 ? 10 : scoreNumber >= 100 && scoreNumber < 200 ? 20 : 30;
+const randomOne = Math.floor(Math.random() * random)
+const randomTwo = Math.floor(Math.random() * random)
+const randomOperator = Math.floor(Math.random() * 4)
 
 const createRandomNumber = ()=> {
     const random = scoreNumber < 100 ? 10 : scoreNumber >= 100 && scoreNumber<200 ? 20 : 30;
     const randomOne  = Math.floor(Math.random() * random)
     const randomTwo = Math.floor(Math.random() * random)
     const randomOperator = Math.floor(Math.random() * 4)
-    assignRandomToElement(randomOne,randomTwo,randomOperator)
+    assignRandomNumToElement(randomOne,randomTwo,randomOperator)
 }
 
-const assignRandomToElement = (randomOne, randomTwo, randomOperator)=> {
+const assignRandomNumToElement = (randomOne, randomTwo, randomOperator)=> {
     let result ;
+    const firstNumber = document.querySelector('.num-one');
+    const secondNumber = document.querySelector('.num-two');
+    const operator = document.querySelector('.operator');
+
     firstNumber.innerHTML = randomOne;
     secondNumber.innerHTML = randomTwo;
     operator.innerHTML = randomOperator == 0 ? '+' : randomOperator == 1 ? '-' : randomOperator == 2 ? '*' : '/'
@@ -32,6 +37,7 @@ const assignRandomToElement = (randomOne, randomTwo, randomOperator)=> {
     else
       result = eval(`${firstNumber.innerHTML}${operator.innerHTML}${secondNumber.innerHTML}`)
 }
+assignRandomNumToElement(randomOne, randomTwo,randomOperator)
 
 const addAnswerToOutputScreen = (e)=> {
     let num = e.target.innerHTML
@@ -66,44 +72,52 @@ const clearOutput = () => {
 }
 
 const createladerboardElement = (dataList) => {
+    let i = 1;
+    const playerInfoEle = document.querySelector('.player')
+    playerInfoEle.innerHTML = ''
     dataList.forEach(data => {
-        const div = document.createElement('div');
+        let div = document.createElement('div');
         div.className = 'player-info';
         div.innerHTML =
-            `
-                <p>${data.id}</p>
-                <p>${data.name}</p>
-                <p>${data.score}</p>
-             `
-        document.querySelector('.player').append(div)
-            })
+        `
+        <p>${i++}</p>
+        <p>${data.name}</p>
+        <p>${data.score}</p>
+        `
+        playerInfoEle.append(div)
+    })
 }
 
 
 const addData = ()=> {
+    const inputName = document.querySelector('input');
     const dataObj = {
         id : (Date.now()) / 2 ,
         name: inputName.value,
         score: scoreNumber
     }
+    setScoreInSortedPosition(dataObj)
+    inputName.value = ''
+    submitBtn.disabled = true 
+}
+
+const setScoreInSortedPosition = (dataObj) =>{
     let index;
     for (let i = dataList.length - 1; i >= 0; i--)
         if (dataList[i].score > dataObj.score) {
             index = dataList.indexOf(dataList[i])
         }
-    if(index != undefined) {
-    dataList.splice(index, 0, dataObj)
-        createladerboardElement(dataList)
+    if (index != undefined) {
+        dataList.splice(index, 0, dataObj)
+        createladerboardElement(dataList) 
         storeDataAtStorage(dataList)
-}
+    }
 
-    else{
+    else {
         dataList.push(dataObj)
         createladerboardElement(dataList)
         storeDataAtStorage(dataList)
     }
-    inputName.value = ''
-    submitBtn.disabled = true 
 }
 
 const storeDataAtStorage = (dataList)=> {
@@ -126,7 +140,6 @@ const init = () => {
     displayData(dataList)
 }
 init()
-
 
 
 enterBtn.addEventListener('click',checkAnswer)
